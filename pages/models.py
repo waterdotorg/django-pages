ad403ab3026b45989b48bd13fb5c0948bde8d84e
Vince Carney <vincecarney@gmail.com>
2015-02-27 09:20:10 -0600

    Invalidate page cache on model save. Bump version 0.2.0

--- pages/models.py
+++ pages/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.conf import settings
+from django.core.cache import cache
 from django.core.urlresolvers import get_script_prefix
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import iri_to_uri, python_2_unicode_compatible
@@ -42,6 +43,10 @@ class Page(models.Model):
 
     def __str__(self):
         return "%s -- %s" % (self.url, self.title)
+
+    def save(self, *args, **kwargs):
+        cache.delete(self.get_absolute_url())
+        super(Page, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         # Handle script prefix manually because we bypass reverse()
